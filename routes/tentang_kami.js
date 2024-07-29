5b939cca48b70b563209965dde524a930ecdae2c
RaihanMuftiMubarok <raihanmiqdad203@.com>
2024-07-29 14:57:41 +0700

    tentang kami

--- routes/tentang_kami.js
+++ routes/tentang_kami.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/tentang_kami', async function (req, res, next) {
+router.get('/users', async function (req, res, next) {
     try {
-
-        res.render('tentang_kami/index', {
+        // let level_users = req.session.level;
+        res.render('tentang_kami/users/index', {
         })
     } catch (error) {
         console.error("Error:", error);
