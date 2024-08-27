f0a29b725d8d36c6cf177d99141ad789ad921432
AldyGege <youngfather86@gmail.com>
2024-08-27 14:36:18 +0700

    CRUD

---
+++ Model/Model_Buku.js
@@ -0,0 +1,69 @@
+const connection = require('../config/database');
+
+class Model_Buku {
+
+    static async getAll(){
+        return new Promise((resolve, reject) => {
+            connection.query('select * from buku order by id_buku desc', (err, rows) => {
+                if(err){
+                    reject(err);
+                } else {
+                    resolve(rows);
+                }
+            });
+        });
+    }
+
+    static async Store(Data){
+        return new Promise((resolve, reject) => {
+            connection.query('insert into buku set ?', Data, function(err, result){
+                if(err){
+                    reject(err);
+                } else {
+                    resolve(result);
+                }
+            })
+        });
+    }
+
+    static async getId(id){
+        return new Promise((resolve, reject) => {
+            connection.query('select * from buku where id_buku = ' + id, (err,rows) => {
+                if(err) {
+                    reject(err);
+                } else {
+                    resolve(rows);
+                }
+            })
+        })
+    }
+
+    static async Update(id, Data) {
+        return new Promise((resolve, reject) => {
+            connection.query('update buku set ? where id_buku =' + id, Data, function(err, result){
+                if(err){
+                    reject(err);
+                    console.log(err);
+                } else {
+                    resolve(result);
+                }
+            })
+        });
+    }
+
+    static async Delete(id) {
+        return new Promise((resolve, reject) => {
+            connection.query('delete from buku where id_buku =' + id, function(err,result){
+                if(err) {
+                    reject(err);
+                } else {
+                    resolve(result);
+                }
+            })
+        });
+    }
+
+}
+
+
+module.exports = Model_Buku;
