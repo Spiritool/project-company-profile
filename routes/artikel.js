68a67d21b5b807ad5c0aad879055dee678503bbe
AldyGege <youngfather86@gmail.com>
2024-09-03 19:54:38 +0700

    tampil data berita

--- routes/artikel.js
+++ routes/artikel.js
@@ -1,16 +1,18 @@
 var express = require('express');
 var router = express.Router();
+const Model_Berita = require('../model/Model_Berita.js');
 
 router.get('/users', async function (req, res, next) {
     try {
-        // let level_users = req.session.level;
+        let rows = await Model_Berita.getAll();
         res.render('artikel/users/index', {
-        })
+            data: rows // Data berita yang akan dirender
+        });
     } catch (error) {
         console.error("Error:", error);
         req.flash('invalid', 'Terjadi kesalahan saat memuat data pengguna');
         res.redirect('/login');
     }
-
 });
+
 module.exports = router;
